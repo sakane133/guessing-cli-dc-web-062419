6781043d560db28681612da672c7acfaa4d29870
Samantha Kane <s.kane133@gmail.com>
2019-06-22 16:14:07 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,6 +7,7 @@ def run_guessing_game
     puts "Goodbye!"
     elsif user_input == number
     puts "You guessed the correct number!"
-    
+  else 
+    puts "The computer guessed #{number}."
   end
 end 
