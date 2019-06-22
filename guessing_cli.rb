650c9916047f8f6264c9023e18f3c5e16a6dcbdf
Samantha Kane <s.kane133@gmail.com>
2019-06-22 16:12:15 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,6 +6,6 @@ def run_guessing_game
   if user_input == "exit"
     puts "Goodbye!"
     elsif user_input == number
-    puts :You guessed the correct number!
+    puts "You guessed the correct number!"
   end
 end 
